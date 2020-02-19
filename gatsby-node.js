232726e4518612f14455ad2d103c5f70a8a4fd55
Chris DeSilva <chris.a.desilva@gmail.com>
2020-02-19 09:45:45 -0600

    Set up redirects from all current subdomains to new subdomain

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,87 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+exports.createPages = ({ graphql, actions }) => {
+	const { createPage, createRedirect } = actions;
 
-// You can delete this file if you're not using it
+	createRedirect({
+		fromPath: 'https://thinkful-da.skills.fund/*',
+		toPath: 'https://thinkful.skills.fund/:splat',
+		isPermanent: true,
+		force: true
+	});
+	createRedirect({
+		fromPath: 'https://thinkful-dai.skills.fund/*',
+		toPath: 'https://thinkful.skills.fund/:splat',
+		isPermanent: true,
+		force: true
+	});
+	createRedirect({
+		fromPath: 'https://thinkful-danw.skills.fund/*',
+		toPath: 'https://thinkful.skills.fund/:splat',
+		isPermanent: true,
+		force: true
+	});
+	createRedirect({
+		fromPath: 'https://thinkful-ds.skills.fund/*',
+		toPath: 'https://thinkful.skills.fund/:splat',
+		isPermanent: true,
+		force: true
+	});
+	createRedirect({
+		fromPath: 'https://thinkful-dsi.skills.fund/*',
+		toPath: 'https://thinkful.skills.fund/:splat',
+		isPermanent: true,
+		force: true
+	});
+	createRedirect({
+		fromPath: 'https://thinkful-dsnw.skills.fund/*',
+		toPath: 'https://thinkful.skills.fund/:splat',
+		isPermanent: true,
+		force: true
+	});
+	createRedirect({
+		fromPath: 'https://thinkful-dmf.skills.fund/*',
+		toPath: 'https://thinkful.skills.fund/:splat',
+		isPermanent: true,
+		force: true
+	});
+	createRedirect({
+		fromPath: 'https://thinkful-ei.skills.fund/*',
+		toPath: 'https://thinkful.skills.fund/:splat',
+		isPermanent: true,
+		force: true
+	});
+	createRedirect({
+		fromPath: 'https://thinkful-fs.skills.fund/*',
+		toPath: 'https://thinkful.skills.fund/:splat',
+		isPermanent: true,
+		force: true
+	});
+	createRedirect({
+		fromPath: 'https://thinkful-nw.skills.fund/*',
+		toPath: 'https://thinkful.skills.fund/:splat',
+		isPermanent: true,
+		force: true
+	});
+	createRedirect({
+		fromPath: 'https://thinkful-pm.skills.fund/*',
+		toPath: 'https://thinkful.skills.fund/:splat',
+		isPermanent: true,
+		force: true
+	});
+	createRedirect({
+		fromPath: 'https://thinkful-pdf.skills.fund/*',
+		toPath: 'https://thinkful.skills.fund/:splat',
+		isPermanent: true,
+		force: true
+	});
+	createRedirect({
+		fromPath: 'https://thinkful-uxui.skills.fund/*',
+		toPath: 'https://thinkful.skills.fund/:splat',
+		isPermanent: true,
+		force: true
+	});
+	createRedirect({
+		fromPath: '/go',
+		toPath: '/?utm_source=school&utm_medium=printedskfbrochure&utm_campaign=studentbrochure'
+	});
+	createRedirect({ fromPath: '/accepted', toPath: '/' });
+};
